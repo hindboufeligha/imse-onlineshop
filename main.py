5243662f285cc69a9a75cc78db503b600b6f33e7
hindboufeligha <139365666+hindboufeligha@users.noreply.github.com>
2023-12-22 20:55:01 +0100

    DATABASE CREATED SUCCESSFULLY

--- main.py
+++ main.py
@@ -25,17 +25,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///onlineshop_.db"  # SQLite URI
 db = SQLAlchemy(app)
 
 
-
+# Join table for N:M relationship between Customers and Products
+customer_product_association = db.Table(
+    "customer_product_association",
+    db.Column("customerID", db.Integer, db.ForeignKey("customer_table.customer_id")),
+    db.Column("productID", db.Integer, db.ForeignKey("product_table.p_id")),
+)
 
 # association table for Wishlist and Product
 wishlist_product_association = db.Table(
     "wishlist_product_association",
-    db.Column("wishlistID", db.Integer, db.ForeignKey("WishlistTable.wishlist_id")),
-    db.Column("productID", db.Integer, db.ForeignKey("ProductTable.p_id")),
+    db.Column("wishlistID", db.Integer, db.ForeignKey("wishlist_table.wishlist_id")),
+    db.Column("productID", db.Integer, db.ForeignKey("product_table.p_id")),
 )
 
 
 class CustomerTable(db.Model):
+    __tablename__ = "customer_table"
     customer_id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(255))
     familyname = db.Column(db.String(255))
@@ -47,7 +53,7 @@ class CustomerTable(db.Model):
     products = db.relationship(
         "ProductTable",
         secondary=customer_product_association,
-        backref="customers",
+        backref="associated_customers",
         lazy=True,
     )
 
@@ -58,6 +64,7 @@ class CustomerTable(db.Model):
 
 
 class AddressTable(db.Model):
+    __tablename__ = "address_table"
     address_id = db.Column(db.Integer, primary_key=True)
     address_title = db.Column(db.String(255))
     street_name = db.Column(db.String(255))  # home / office / other
@@ -69,11 +76,12 @@ class AddressTable(db.Model):
     country = db.Column(db.String(12))
 
     customer_id = db.Column(
-        db.Integer, db.ForeignKey("CustomerTable.customer_id"), nullable=False
+        db.Integer, db.ForeignKey("customer_table.customer_id"), nullable=False
     )
 
 
 class ProductTable(db.Model):
+    __tablename__ = "product_table"
     p_id = db.Column(db.Integer, primary_key=True)
     p_name = db.Column(db.String(255))
     p_description = db.Column(db.String(255))
@@ -82,26 +90,27 @@ class ProductTable(db.Model):
     p_size_variation = db.Column(db.String(255))
 
     reviews = db.relationship("ReviewTable", backref="product", lazy=True)
+    customers = db.relationship(
+        "CustomerTable",
+        secondary=customer_product_association,
+        backref="associated_products",
+        lazy=True,
+    )
 
     category_id = db.Column(
-        db.Integer, db.ForeignKey("CategoryTable.category_id"), nullable=False
+        db.Integer, db.ForeignKey("category_table.category_id"), nullable=False
     )
 
     cart_items = db.relationship("CartItemTable", backref="product", lazy=True)
 
-# Join table for N:M relationship between Customers and Products
-customer_product_association = db.Table(
-    "customer_product_association",
-    db.Column("customerID", db.Integer, db.ForeignKey("CustomerTable.customer_id")),
-    db.Column("productID", db.Integer, db.ForeignKey("ProductTable.p_id")),
-)
 
 class CategoryTable(db.Model):
+    __tablename__ = "category_table"
     category_id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.Text)
     parent_category_id = db.Column(
-        db.Integer, db.ForeignKey("CategoryTable.category_id")
+        db.Integer, db.ForeignKey("category_table.category_id")
     )
 
     subcategories = db.relationship(
@@ -112,12 +121,13 @@ class CategoryTable(db.Model):
 
 
 class CartTable(db.Model):
+    __tablename__ = "cart_table"
     cart_id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(20), nullable=False)
     creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     customer_id = db.Column(
-        db.Integer, db.ForeignKey("CustomerTable.customer_id"), nullable=False
+        db.Integer, db.ForeignKey("customer_table.customer_id"), nullable=False
     )
 
     cart_items = db.relationship("CartItemTable", backref="cart", lazy=True)
@@ -125,48 +135,52 @@ class CartTable(db.Model):
 
 
 class CartItemTable(db.Model):
+    __tablename__ = "cartitem_table"
     cart_item_id = db.Column(db.Integer, primary_key=True)
     size = db.Column(db.String(10))
     quantity = db.Column(db.Integer, nullable=False)
 
-    cart_id = db.Column(db.Integer, db.ForeignKey("CartTable.cart_id"), nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey("cart_table.cart_id"), nullable=False)
     product_id = db.Column(
-        db.Integer, db.ForeignKey("ProductTable.p_id"), nullable=False
+        db.Integer, db.ForeignKey("product_table.p_id"), nullable=False
     )
 
 
 class PaymentTable(db.Model):  # 1:1 with Cart
+    __tablename__ = "payment_table"
     payment_id = db.Column(db.Integer, primary_key=True)
     payment_method = db.Column(db.String(50), nullable=False)
     total_cost = db.Column(db.Float, nullable=False)
     payment_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     cart_id = db.Column(
-        db.Integer, db.ForeignKey("CartTable.cart_id"), unique=True, nullable=False
+        db.Integer, db.ForeignKey("cart_table.cart_id"), unique=True, nullable=False
     )
 
 
 class ReviewTable(db.Model):
+    __tablename__ = "review_table"
     ReviewID = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
     rating = db.Column(db.Float, nullable=False)
     post_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     customer_id = db.Column(
-        db.Integer, db.ForeignKey("CustomerTable.customer_id"), nullable=False
+        db.Integer, db.ForeignKey("customer_table.customer_id"), nullable=False
     )
 
     product_id = db.Column(
-        db.Integer, db.ForeignKey("ProductTable.p_id"), nullable=False
+        db.Integer, db.ForeignKey("product_table.p_id"), nullable=False
     )
 
 
 class WishlistTable(db.Model):
+    __tablename__ = "wishlist_table"
     wishlist_id = db.Column(db.Integer, primary_key=True)
     wishlist_name = db.Column(db.String(50), nullable=False)
 
     customer_id = db.Column(
-        db.Integer, db.ForeignKey("CustomerTable.customer_id"), nullable=False
+        db.Integer, db.ForeignKey("customer_table.customer_id"), nullable=False
     )
 
     products = db.relationship(
