9b3a6e36d3dbba1954da7545956405c2806b861c
hindboufeligha <139365666+hindboufeligha@users.noreply.github.com>
2024-01-11 15:40:51 +0100

    Created migration_functions and database_functions TO ORGANIZE THE CODE

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import FlushError
 from sqlalchemy import func, desc
 from datetime import datetime, timedelta
+from flask_pymongo import PyMongo
+from bson import ObjectId
+from lib.database_functions import *
 
 app = Flask(__name__, static_folder="assets", template_folder="templates")
 app.config["SESSION_TYPE"] = "filesystem"
@@ -111,7 +114,7 @@ def empty_database():
 
 @app.route("/my_reviews")
 def reviews():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
     if user_id:
         user_data = CustomerTable.query.filter_by(customer_id=user_id).first()
         if not user_data:
@@ -119,11 +122,12 @@ def reviews():
             return "User data not found", 404
 
         user_reviews = ReviewTable.query.filter_by(customer_id=user_id).all()
-        return render_template("reviews.html", reviews=user_reviews, user_data=user_data)
+        return render_template(
+            "reviews.html", reviews=user_reviews, user_data=user_data
+        )
     else:
         # Redirect to login if no user is in session
         return redirect(url_for("show_login"))
-
 
 
 @app.route("/delete_review/<int:review_id>", methods=["POST"])
@@ -132,17 +136,14 @@ def delete_review(review_id):
     if review:
         db.session.delete(review)
         db.session.commit()
-        return jsonify({'message': 'Review deleted successfully'}), 200
+        return jsonify({"message": "Review deleted successfully"}), 200
     else:
-        return jsonify({'message': 'Review not found'}), 404
-
-
-
+        return jsonify({"message": "Review not found"}), 404
 
 
 @app.route("/add-review/<int:product_id>", methods=["GET", "POST"])
 def add_review(product_id):
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
     if not user_id:
         return redirect(url_for("show_login"))
 
@@ -156,12 +157,14 @@ def add_review(product_id):
         flash("Product not found", "error")
         return redirect(url_for("index"))
 
-    existing_review = ReviewTable.query.filter_by(customer_id=user_id, product_id=product_id).first()
+    existing_review = ReviewTable.query.filter_by(
+        customer_id=user_id, product_id=product_id
+    ).first()
 
     if request.method == "POST":
-        title = request.form.get('title')
-        description = request.form.get('description')
-        rating = float(request.form.get('rating'))
+        title = request.form.get("title")
+        description = request.form.get("description")
+        rating = float(request.form.get("rating"))
 
         if existing_review:
             existing_review.title = title
@@ -175,7 +178,7 @@ def add_review(product_id):
                 rating=rating,
                 post_date=datetime.utcnow(),
                 customer_id=user_id,
-                product_id=product_id
+                product_id=product_id,
             )
             db.session.add(new_review)
 
@@ -183,39 +186,42 @@ def add_review(product_id):
         flash("Review saved successfully", "success")
         return redirect(url_for("reviews"))
 
-    return render_template("add-review.html", user_data=user_data, product=product, review=existing_review)
-
-
-    
+    return render_template(
+        "add-review.html", user_data=user_data, product=product, review=existing_review
+    )
 
 
 @app.route("/submit_review", methods=["POST"])
 def submit_review():
     # Retrieve form data
-    title = request.form.get('title')
-    description = request.form.get('description')
-    rating = request.form.get('rating')
-    product_id = request.form.get('product_id')  # Ensure this hidden field is in your form
+    title = request.form.get("title")
+    description = request.form.get("description")
+    rating = request.form.get("rating")
+    product_id = request.form.get(
+        "product_id"
+    )  # Ensure this hidden field is in your form
 
     if not product_id:
         return "Product ID is required", 400
 
     # Process image upload if available
-    image = request.files.get('image')
-    if image and image.filename != '':
+    image = request.files.get("image")
+    if image and image.filename != "":
         filename = secure_filename(image.filename)
-        image_save_directory = os.path.join(app.static_folder, 'uploads/reviews')
+        image_save_directory = os.path.join(app.static_folder, "uploads/reviews")
         os.makedirs(image_save_directory, exist_ok=True)
 
         image_path = os.path.join(image_save_directory, filename)
         image.save(image_path)
 
-        image_url = url_for('static', filename=f'uploads/reviews/{filename}')
+        image_url = url_for("static", filename=f"uploads/reviews/{filename}")
     else:
         image_url = None  # Handle the case when no image is uploaded
 
     # Check for existing review
-    existing_review = ReviewTable.query.filter_by(customer_id=session.get('user_id'), product_id=product_id).first()
+    existing_review = ReviewTable.query.filter_by(
+        customer_id=session.get("user_id"), product_id=product_id
+    ).first()
 
     if existing_review:
         # Update the existing review
@@ -231,8 +237,8 @@ def submit_review():
             description=description,
             rating=rating,
             image_url=image_url,
-            customer_id=session.get('user_id'),
-            product_id=product_id
+            customer_id=session.get("user_id"),
+            product_id=product_id,
         )
         db.session.add(new_review)
         db.session.commit()
@@ -240,35 +246,38 @@ def submit_review():
     return "Review submitted successfully"
 
 
-
-
 @app.route("/order_list")
 def order_list():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
     if user_id:
         user_data = CustomerTable.query.filter_by(customer_id=user_id).first()
         if user_data:
             # Query to find product IDs associated with the user
-            associated_product_ids = db.session.query(customer_product_association.c.productID).filter(
-                customer_product_association.c.customerID == user_id
-            ).all()
+            associated_product_ids = (
+                db.session.query(customer_product_association.c.productID)
+                .filter(customer_product_association.c.customerID == user_id)
+                .all()
+            )
 
             # Fetch the products using the retrieved product IDs
             product_ids = [pid[0] for pid in associated_product_ids]
-            associated_products = ProductTable.query.filter(ProductTable.p_id.in_(product_ids)).all()
+            associated_products = ProductTable.query.filter(
+                ProductTable.p_id.in_(product_ids)
+            ).all()
 
             # Pass both user_data and products to the template
-            return render_template("order_list.html", user_data=user_data, products=associated_products)
+            return render_template(
+                "order_list.html", user_data=user_data, products=associated_products
+            )
         else:
             return "User data not found", 404
     else:
         return redirect(url_for("show_login"))
 
 
-
 @app.route("/search_products", methods=["GET", "POST"])
 def search_products():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
     if user_id:
         user_data = CustomerTable.query.filter_by(customer_id=user_id).first()
         if not user_data:
@@ -276,14 +285,17 @@ def search_products():
 
         if request.method == "POST":
             search_query = request.form.get("searchQueryInput", "")
-            matched_products = ProductTable.query.filter(ProductTable.p_name.like(f"%{search_query}%")).all()
-            return render_template("order_list.html", user_data=user_data, products=matched_products)
+            matched_products = ProductTable.query.filter(
+                ProductTable.p_name.like(f"%{search_query}%")
+            ).all()
+            return render_template(
+                "order_list.html", user_data=user_data, products=matched_products
+            )
         else:
             return render_template("order_list.html", user_data=user_data)
 
     else:
         return redirect(url_for("show_login"))
-
 
 
 # Route to display login page
@@ -362,6 +374,7 @@ def signup():
 def DB_operation():
     return render_template("DB_operation.html")
 
+
 @app.route("/index")
 def index():
     user_id = session.get("user_id")
@@ -371,28 +384,28 @@ def index():
             # Function to get top products based on gender
             def get_top_products(gender):
                 six_months_ago = datetime.utcnow() - timedelta(days=180)
-                avg_ratings = db.session.query(
-                    ReviewTable.product_id,
-                    func.avg(ReviewTable.rating).label('average_rating')
-                ).filter(
-                    ReviewTable.post_date >= six_months_ago
-                ).group_by(
-                    ReviewTable.product_id
-                ).subquery()
+                avg_ratings = (
+                    db.session.query(
+                        ReviewTable.product_id,
+                        func.avg(ReviewTable.rating).label("average_rating"),
+                    )
+                    .filter(ReviewTable.post_date >= six_months_ago)
+                    .group_by(ReviewTable.product_id)
+                    .subquery()
+                )
 
-                return db.session.query(
-                    ProductTable, avg_ratings.c.average_rating
-                ).join(
-                    avg_ratings, ProductTable.p_id == avg_ratings.c.product_id
-                ).filter(
-                    ProductTable.p_gender == gender
-                ).order_by(
-                    avg_ratings.c.average_rating.desc()
-                ).limit(5).all()
+                return (
+                    db.session.query(ProductTable, avg_ratings.c.average_rating)
+                    .join(avg_ratings, ProductTable.p_id == avg_ratings.c.product_id)
+                    .filter(ProductTable.p_gender == gender)
+                    .order_by(avg_ratings.c.average_rating.desc())
+                    .limit(5)
+                    .all()
+                )
 
-            top_male_products = get_top_products('Male')
-            top_female_products = get_top_products('Female')
-            top_kids_products = get_top_products('Children')
+            top_male_products = get_top_products("Male")
+            top_female_products = get_top_products("Female")
+            top_kids_products = get_top_products("Children")
 
             # Pass user_data and top products to the template
             return render_template(
@@ -400,7 +413,7 @@ def index():
                 user_data=user_data,
                 top_male_products=top_male_products,
                 top_female_products=top_female_products,
-                top_kids_products=top_kids_products
+                top_kids_products=top_kids_products,
             )
         else:
             # Handle case where user data is not found
@@ -408,7 +421,6 @@ def index():
     else:
         # Redirect to login if no user is in session
         return redirect(url_for("show_login"))
-
 
 
 @app.route("/psubcategory")
@@ -613,58 +625,9 @@ def cart_display():
 ### Route to add items to the Cart: Backend ###
 @app.route("/add_to_cart", methods=["POST"])
 def add_to_cart():
+    customer_id = session.get("user_id")
     try:
-        customer_id = session.get("user_id")
-        # print("customer ID:", customer_id)
-        product_id = request.json["product_id"]
-        # print("product ID:", product_id)
-        size_name = request.json["size_name"]
-        # print("size name:", size_name)
-        selected_quantity = request.json["selected_quantity"]
-        # print("selected quantity: ", selected_quantity)
-
-        # fetch the size id of the selected size:
-        size_id = SizeTable.query.filter_by(size_name=size_name).first()
-        if size_id is None:
-            return jsonify({"error": "Invalid size name"}), 400
-
-        # first, check if the customer has an Active cart or not:
-        cart = CartTable.query.filter_by(
-            customer_id=customer_id, status="Active"
-        ).first()
-
-        if cart is None:  # no active cart
-            # then we create one:
-            cart = CartTable(
-                customer_id=customer_id,
-                status="Active",
-                creation_date=datetime.utcnow(),
-            )
-            db.session.add(cart)
-            db.session.commit()
-
-        # check if the product with the same size is already in the cart for the customer:
-        existing_cart_item = CartItemTable.query.filter_by(
-            cart_id=cart.cart_id, size=size_name, product_id=product_id
-        ).first()
-
-        if existing_cart_item:
-            # the product is already in the cart, then update the quantity:
-            existing_cart_item.quantity += selected_quantity
-
-        else:
-            # if not then add the item and update the db (CartItemTable)
-            new_cart_item = CartItemTable(
-                cart_id=cart.cart_id,
-                product_id=product_id,
-                size=size_name,
-                quantity=selected_quantity,
-            )
-
-            db.session.add(new_cart_item)
-
-        db.session.commit()
-
+        addToCart(customer_id, request, db)
         return jsonify({"message": "Item added to the cart successfully!"}), 200
 
     except IntegrityError:
